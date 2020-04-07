af9baaccf163c7b2ae7ae716f0c87ecc6fdf55f0
dnlsfq <dan90@live.com.my>
2020-04-08 01:43:34 +0800

    updated

--- app.py
+++ app.py
@@ -48,16 +48,20 @@ app.title=tabtitle
 
 ########### Set up the layout
 app.layout = html.Div(children=[
+    
     html.H1(myheading),
+    
     dcc.Graph(
         id='flyingdog',
         figure=beer_fig
     ),
+    
     html.A('Code on Github', href=githublink),
+    
     html.Br(),
+    
     html.A('Data Source', href=sourceurl),
-    ]
-)
+])
 
 if __name__ == '__main__':
     app.run_server()
